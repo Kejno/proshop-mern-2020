5af0fa0ca99bc4447167f2cc09fff94dce889bb6
Artem Rimarchyk <1patrik@mail.ru>
2020-10-19 11:41:44 +0300

    feat: implement action removeFromCart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addCart } from '../actions/cartActions'
+import { addCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -20,12 +20,13 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty])
 
-  const removeFromCartHandler = () => {
-    console.log('remove')
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = () => {
     history.push('/login?redirect=shipping')
+    console.log(history)
   }
 
   return (
